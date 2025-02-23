33bccec3916443ffdba99bb98366d241b87041e9
tamer79 <tamer79@gmail.com>
2025-02-23 14:37:51 +0800

    Tentando acertar novamente...

--- api/models.py
+++ api/models.py
@@ -5,5 +5,6 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)  # 🔹 Adicionado
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
